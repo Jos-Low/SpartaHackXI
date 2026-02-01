81380360bc603c35f952c297663d65d035842f45
owencurt <owenpcurtis@outlook.com>
2026-02-01 03:55:29 -0500

    Descriptions for the games

--- gameConfig.js
+++ gameConfig.js
@@ -12,11 +12,11 @@ export const GAME_CONFIG = {
       upgradeMinigame: "minigames/game_1/game_1.html",
       upgradeIntroVideo: "assets/videos/CutSceneOne.mp4",
       upgradeHowTo: {
-        title: "How to play: Game 1",
+        title: "How to play: Flower Run",
         bullets: [
-          "Do the thing.",
-          "Avoid the other thing.",
-          "Win by reaching the goal."
+          "Press space to jump over the flowers,",
+          "But be careful to not jump into the butterflies...",
+          "Use the SPACE key to jump."
         ]
       }
     },
@@ -28,25 +28,64 @@ export const GAME_CONFIG = {
       // when upgrading FROM level 2 TO 3, use this minigame
       // (your actual file is game2.html, NOT game_2.html)
 
-      upgradeMinigame: "minigames/game_2/game_2.html"
+      upgradeMinigame: "minigames/game_2/game_2.html",
+      upgradeIntroVideo: "assets/videos/CutSceneOne.mp4",
+      upgradeHowTo: {
+        title: "How to play: Cloud Jump",
+        bullets: [
+          "You can't stop jumping!",
+          "Make sure to land on clouds to not fall to your death...",
+          "Move using the 'A' and 'D' keys."
+        ]
+      }
     },
     {
       level: 3,
       characterIcon: "assets/character/LVL_3_Animated_Flower.png",
       xpToNext: 200,
-      upgradeMinigame: "minigames/game_3/game_3.html"
+      upgradeMinigame: "minigames/game_3/game_3.html",
+      upgradeIntroVideo: "assets/videos/CutSceneOne.mp4",
+      upgradeHowTo: {
+        title: "How to play: Wire Match",
+        bullets: [
+          "Connect the same colored dots to eachother.",
+          "Make sure that you fill up every empty space!",
+          "Drag lines using your mouse and left click.",
+          "Complete 3 challenges and pass the test."
+        ]
+      }
     },
     {
       level: 4,
       characterIcon: "assets/character/LVL_4_Animated_Flower.png",
       xpToNext: 300,
-      upgradeMinigame: "minigames/game_4/game_4.html"
+      upgradeMinigame: "minigames/game_4/game_4.html",
+      upgradeIntroVideo: "assets/videos/CutSceneOne.mp4",
+      upgradeHowTo: {
+        title: "How to play: Memory",
+        bullets: [
+          "The cards will be shown for a short time,",
+          "Pay attention before they flip over..",
+          "Click on the corresponding images to get them all up.",
+          "But be careful, 3 wrong guesses and it's all over..."
+        ]
+      }
     },
     {
       level: 5,
       characterIcon: "assets/character/LVL_5_Animated_Flower.png",
       xpToNext: 400,
-      upgradeMinigame: "minigames/game_5/game_5.html"
+      upgradeMinigame: "minigames/game_5/game_5.html",
+      upgradeIntroVideo: "assets/videos/CutSceneOne.mp4",
+      upgradeHowTo: {
+        title: "How to play: Dumpster Diving",
+        bullets: [
+          "WARNING: DO NOT HARM THE RACCOONS",
+          "Hit the evil sloth to wear out his health.",
+          "Get him 10 times to scare him out of the trash.",
+          "Make sure that you get him before he hides!"
+        ]
+      }
     },
     {
       level: 6,
